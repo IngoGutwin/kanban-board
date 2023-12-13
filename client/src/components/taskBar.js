d76585f02fb31342febc6856a1b0d6779a4052a8
IngoGutwin <onmabu@gmail.com>
2023-12-13 15:45:34 +0100

    Add button for new columns

--- client/src/components/taskBar.js
+++ client/src/components/taskBar.js
@@ -2,7 +2,7 @@ export function emptyBoard() {
   return `
     <div class="task-bar-empty-board" id="task-empty-board">
       <h2 class="task-bar-heading">This board is empty. Create a new column to get started.</h2>
-      <button class="task-bar-add-new-column" id="task-bar-add-new-columns"></button>
+      <button class="primary-btn-L" id="task-bar-add-new-columns">+ Add New Column</button>
     </div>
   `;
 }
